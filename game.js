2809c3d71b02b1fcc627cd051e21fc92019e6f5d
Jiming Zheng <jimingz9380@gmail.com>
2022-07-23 13:09:29 -0400

    add rebound shield; everything functional

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 let blueXCor;
 let redXCor;
 
-let xPos;
-let yPos;
+let ballX;
+let ballY;
 
 let xSpeed;
 let ySpeed;
@@ -23,8 +23,8 @@ function setup(){
     blueXCor = 375;
     redXCor = 375;
 
-    xPos = 375
-    yPos = 450
+    ballX = 375
+    ballY = 450
 
     xSpeed = random(1, 6);
     ySpeed = random(1, 6);
@@ -33,11 +33,16 @@ function setup(){
     p2Score = 0;
 
     rectMode(CENTER);
+    textAlign(CENTER);
 }
 
 function draw(){
     background(0);
     
+    //ball speed
+    ballX += xSpeed * xDirection;
+    ballY += ySpeed * yDirection;
+
     //top and bottom border
     fill (40, 70, 100);
     rect (375, 10, 750, 20);
@@ -45,29 +50,49 @@ function draw(){
 
     //the ball
     fill(50, 50, 50);
-    ellipse(xPos, yPos, 50, 50);
+    ellipse(ballX, ballY, 50, 50);
 
-    //player1 shield
+    //player1red shield
     fill(255, 0, 0);
     rect(redXCor, 100, 150, 20);
 
-    //player2 shield
+    //player2blue shield
     fill(0, 0, 255);
     rect(blueXCor, 800, 150, 20);
 
-    xPos += xSpeed * xDirection;
-    yPos += ySpeed * yDirection;
+    //woring on the shield function
+    p1Right = redXCor + 75;
+    p1Left = redXCor - 75;
+
+    p2Right = blueXCor + 75;
+    p2Left = blueXCor - 75;
+
+    if(ballX < p1Right && ballX > p1Left && ballY - 10 <= 100){
+        yDirection *= -1;
+    }
+
+    if(ballX < p2Right && ballX > p2Left && ballY + 10 >= 800){
+        yDirection *= -1;
+    } 
 
     //ball bounce control
-    if (xPos > 750 || xPos < 0) {
-            xDirection *= -1;
+    if (ballX > 750 || ballX < 0) {
+        xDirection *= -1;
     }
 
-    if (yPos > 900 || yPos < 0){
-            yDirection *= -1;
+    if (ballY > 880){
+        yDirection *= -1;
+        p1Score ++;
+        ballY = 450
+        
+    }
+    else if(ballY < 20){
+        yDirection *= -1;
+        p2Score ++;
+        ballY = 450;
     }
 
-    //p1 movement
+    //p1red movement
     if (keyIsDown(RIGHT_ARROW)){
         redXCor += 60;
     }
@@ -76,7 +101,7 @@ function draw(){
         redXCor -= 60;
     }
 
-    //p1 boundary
+    //p1red boundary
     if(redXCor > 700){
         redXCor -=60;
     }
@@ -84,10 +109,10 @@ function draw(){
     if(redXCor < 50 ){
         redXCor += 60;
     }
-
+    //p2blue mouse control movement
     blueXCor = mouseX;
 
-    //p2 boundary
+    //p2blue boundary
     if (blueXCor > 700){
         blueXCor -=60;
     }
@@ -96,103 +121,30 @@ function draw(){
         blueXCor +=60
     }
 
+    //scores and position 
     fill(255);
     textSize(18);
-    text("Blue Score:" + p1Score, 25, 450)
+    text("Blue Score:" + p1Score, 75, 450)
 
     fill(255);
     textSize(18);
-    text("Red Score:" + p2Score, 625, 450)
+    text("Red Score:" + p2Score, 675, 450)
 
-    p1Right = redXCor += 75;
-    p1Left = redXCor -= 75;
 
-    p2Right = blueXCor += 75;
-    p2Left = blueXCor -= 75;
-
-    if(p1Right < xPos || p1Right > xPos){
-
+    //winner text
+    if(p1Score > 1){
+        background(255, 0, 0);
+        fill(0);
+        textSize(100)
+        text("Red Win!!!",375, 450);
     }
 
-    else{
-        xDirection *= -1;
-        yDirection *= -1;
+    if(p2Score > 1){
+        background(0, 0, 255);
+        fill(0);
+        textSize(100)
+        text("Blue Win!!!",375, 450);
     }
 }
-
-
-// // Anna Wasson
-// // Lab 4: Pong
-// // 2-10-19
-// // Random Ball Placement
-// var xBall = Math.floor(Math.random() * 300) + 50;
-// var yBall = 50;
-// var xSpeed = (2, 7);
-// var ySpeed = (-7, -2);
-// var score = 0
-// // Canvas
-// function setup() {
-//   createCanvas(400, 400);
-// }
-// //Background
-// function draw() {
-//   // Background
-//   background(0);
-//   // Paddle
-//   fill('#FFFFFF');
-//   rect(mouseX, 375, 90, 15);
-//   //Functions
-//   move();
-//   display();
-//   bounce();
-//   //resetBall();
-//   paddle();
-//   //Score
-//   fill('#D9C3F7');
-//   textSize(24);
-//   text("Score: " + score, 10, 25);
-// }
-// // Ball Functions
-// function move() {
-//   xBall += xSpeed;
-//   yBall += ySpeed;
-// }
-// function bounce() {
-//   if (xBall < 10 ||
-//     xBall > 400 - 10) {
-//     xSpeed *= -1;
-//   }
-//   if (yBall < 10 ||
-//     yBall > 400 - 10) {
-//     ySpeed *= -1;
-//   }
-// }
-// // Reset Ball
-// //function resetBall() {
-// //  if (yBall >= 400 ||
-// //    yBall > 400 - 10) {
-// //    ySpeed = 4;
-// // }
-// //}
-// function display() {
-//   fill('#D9C3F7');
-//   ellipse(xBall, yBall, 20, 20);
-// }
-// // Bounce off Paddle
-// function paddle() {
-//   if ((xBall > mouseX &&
-//       xBall < mouseX + 90) &&
-//     (yBall + 10 >= 375)) {
-//     xSpeed *= -1;
-//     ySpeed *= -1;
-//     score++;
-//   }
-
-
-
-
-
-
-
 
 
